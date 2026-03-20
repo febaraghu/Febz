ad39e3ce397eb2013aa20209c287a61138cd532d
febaraghu <febaraghu7@gmail.com>
2026-03-20 11:38:34 +0530

    Describe your changes here

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('', views.homee, name='homee'),  # homepage
     path('shop/', views.shop, name='shop'),
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
     path('cart/', views.cart, name='cart'),
