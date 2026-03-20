2d2eefbd38546faa76a6809b867154282f35e268
febaraghu <febaraghu7@gmail.com>
2026-03-20 11:14:49 +0530

    updated

--- febzcouture/settings.py
+++ febzcouture/settings.py
@@ -31,7 +31,7 @@ ROOT_URLCONF = 'febzcouture.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],   # ✅ FIXED
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
